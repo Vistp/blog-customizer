7bf6b7de455ec4dd76ad63b990cff986d6d8d588
Irina Varushkina <rnvr@mail.ru>
2026-08-26 20:54:10 +0500

    ПР-10 Открытие и закрытие сайдбара по кнопке. фиксирует начальное состояние пропсом

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -20,7 +20,7 @@ export const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm isOpen={true} />
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	isOpen: boolean;
+}
+
+export const ArticleParamsForm = ({
+	isOpen: propIsOpen,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleSidebar = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={propIsOpen} onClick={toggleSidebar} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
