ff50dfc17b3ab521834f0a752e033da2bf6e3f61
Irina Varushkina <rnvr@mail.ru>
2026-08-29 23:15:18 +0500

    ПР-10 Добавляет поля формы и смену их состояний

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,6 +3,17 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
+import {
+	fontFamilyOptions,
+	defaultArticleState,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 interface ArticleParamsFormProps {
 	isOpen: boolean;
@@ -12,19 +23,67 @@ export const ArticleParamsForm = ({
 	isOpen: propIsOpen,
 }: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [selectedFont, setSelectedFont] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [selectedFontSize, setSelectedFontSize] = useState(
+		defaultArticleState.fontSizeOption
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState(
+		defaultArticleState.fontColor
+	);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [selectedContentWidth, setSelectedContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
 
+	const isSidebarOpen = propIsOpen !== undefined ? propIsOpen : isOpen;
 	const toggleSidebar = () => {
 		setIsOpen(!isOpen);
 	};
 
 	return (
 		<>
-			<ArrowButton isOpen={propIsOpen} onClick={toggleSidebar} />
+			<ArrowButton isOpen={isSidebarOpen} onClick={toggleSidebar} />
 			<aside
 				className={`${styles.container} ${
 					isOpen ? styles.container_open : ''
 				}`}>
 				<form className={styles.form}>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={selectedFont}
+						onChange={setSelectedFont}
+					/>
+					<RadioGroup
+						name='fontSize'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={selectedFontSize}
+						onChange={setSelectedFontSize}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={selectedFontColor}
+						onChange={setSelectedFontColor}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={selectedBackgroundColor}
+						onChange={setSelectedBackgroundColor}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={selectedContentWidth}
+						onChange={setSelectedContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
